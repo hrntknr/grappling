8bdcb1da72e32229e806599fa74eb78f7d1da558
Takanori Hirano <hrn.takanori@gmail.com>
2021-01-05 10:34:52 +0900

    disable Fall damage without Effect

--- src/main/java/net.hrntknr.grappling/Grappling.java
+++ src/main/java/net.hrntknr.grappling/Grappling.java
@@ -5,6 +5,7 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
@@ -17,6 +18,7 @@ import org.bukkit.util.Vector;
 
 public class Grappling extends JavaPlugin implements Listener {
   ItemStack grapplingHookItemStack;
+
   public Grappling() {
     grapplingHookItemStack = new ItemStack(Material.FISHING_ROD);
     ItemMeta meta = grapplingHookItemStack.getItemMeta();
@@ -29,7 +31,7 @@ public class Grappling extends JavaPlugin implements Listener {
   }
 
   @Override
-  public void onEnable(){
+  public void onEnable() {
     getServer().getPluginManager().registerEvents(this, this);
     NamespacedKey key = new NamespacedKey(this, "grappling_hook");
     ShapedRecipe recipe = new ShapedRecipe(key, grapplingHookItemStack);
@@ -55,18 +57,25 @@ public class Grappling extends JavaPlugin implements Listener {
       Location playerPos = player.getLocation();
       Location hookPos = event.getHook().getLocation();
       Vector vec = hookPos.toVector().subtract(playerPos.toVector());
-      vec.setY(vec.getY()*0.3);
+      vec.setY(vec.getY() * 0.3);
       player.setVelocity(vec);
-      player.addPotionEffect(
-        new PotionEffect(
-          PotionEffectType.JUMP,
-          100,
-          255,
-          false,
-          false,
-          false
-        )
-      );
     }
+  }
+
+  @EventHandler
+  public void onEntityDamageEvent(EntityDamageEvent event) {
+    if (!(event.getEntity() instanceof Player)) {
+      return;
+    }
+    Player player = (Player) event.getEntity();
+    if (event.getCause() != EntityDamageEvent.DamageCause.FALL) {
+      return;
+    }
+    ItemStack mainHandItem = player.getInventory().getItemInMainHand();
+    ItemStack offHandItem = player.getInventory().getItemInOffHand();
+    if (!mainHandItem.equals(grapplingHookItemStack) && !offHandItem.equals(grapplingHookItemStack)) {
+      return;
+    }
+    event.setCancelled(true);
   }
 }
